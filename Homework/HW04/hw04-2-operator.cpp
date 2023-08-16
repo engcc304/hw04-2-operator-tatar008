6c29b8a19e4c18f19f4d870a3d00629eeb03725c
tatar008 <jirapat_ja66@live.rmutl.ac.th>
2023-08-16 23:28:16 +0700

    hw04-2

--- Homework/HW04/hw04-2-operator.cpp
+++ Homework/HW04/hw04-2-operator.cpp
@@ -25,7 +25,7 @@ int main() {
     printf( "a %%= 1 %d\n", a %= 1 ) ;
     printf( "a %%= b %d\n", a %= b ) ;
     printf( "a && b %d\n", a && b ) ;
-    printf( "a || b %d\n", a || b ) ;
+    printf( "a  b %d\n", a || b ) ;
     return 0 ;
 }
 
